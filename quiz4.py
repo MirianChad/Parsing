0a5aede9577aa1f61bc49c59f1308312ebf5c555
MirianChad <103271526+MirianChad@users.noreply.github.com>
2022-05-27 23:03:20 +0400

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -7,10 +7,10 @@ from random import randint
 
 file = open("result.csv", "w", encoding="utf-8_sig", newline='\n')
 file_obj = csv.writer(file)
-file_obj.writerow(["დასახელება", "მახასიათებლები", "ფასი ლარებში-დოლარებში"])
+file_obj.writerow(["დასახელება", "გარბენი", "გადაცემათა კოლოფი", "ძრავი", "საჭე", "ფასი ლარებში", "ფასი დოლარებში"])
 
 for ind in range(1,7):
-    url = f'https://ss.ge/ka/auto/list/iyideba?Page='+str(ind)+'&AutoManufacturerId=&VehicleModelIds=&VehicleDealTypes=2&YearFrom=&YearTo=&CurrencyId=1&PriceFrom=&PriceTo=&CityIds=&Query=&MillageFrom=&MillageTo=&EngineFrom=&EngineTo=&IsCustomsCleared=&TechnicalInspection=&AutoDealershipId=&El_Windows=false&Hatch=false&CruiseControl=false&StartStopSystem=false&AUX=false&Bluetooth=false&AntiFogHeadlights=false&SeatHeater=false&SmartSeats=false&MultiWheel=false&Signalisation=false&BoardComputer=false&Conditioner=false&ClimateControl=false&RearViewCamera=false&Monitor=false&PanoramicCeiling=false&ParkingControl=false&ABS=false&CentralLocker=false&AutoParking=false&LedHeadlights=false&AdaptedForDisabled=false'
+    url = f'https://ss.ge/ka/auto/list/iyideba?Page={ind}&AutoManufacturerId=&VehicleModelIds=&VehicleDealTypes=2&YearFrom=&YearTo=&CurrencyId=1&PriceFrom=&PriceTo=&CityIds=&Query=&MillageFrom=&MillageTo=&EngineFrom=&EngineTo=&IsCustomsCleared=&TechnicalInspection=&AutoDealershipId=&El_Windows=false&Hatch=false&CruiseControl=false&StartStopSystem=false&AUX=false&Bluetooth=false&AntiFogHeadlights=false&SeatHeater=false&SmartSeats=false&MultiWheel=false&Signalisation=false&BoardComputer=false&Conditioner=false&ClimateControl=false&RearViewCamera=false&Monitor=false&PanoramicCeiling=false&ParkingControl=false&ABS=false&CentralLocker=false&AutoParking=false&LedHeadlights=false&AdaptedForDisabled=false'
     r =  requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     main_body = soup.find('div', id='main-body')
@@ -27,21 +27,26 @@ for ind in range(1,7):
         price_item = car.find('div', class_='r-item')
         price = price_item.find('div', class_='price priceGel')
         price1 = price_item.find('div', class_='price priceUsd')
+
         name = item_decription.a.text
         features  = item_decription.div.text
         pricegel_usd = price_item.div.text
+        feat = features.split('\n')
+        pricesplt = pricegel_usd.split('\n')
 
 
 
-        file_obj.writerow([name, features, pricegel_usd+'$'])
 
-        print(name)
-        print(features)
 
-    # sleep(randint(15, 20))
+        file_obj.writerow([name,feat[1], feat[2], feat[3], feat[4],pricesplt[1]+'₾', pricesplt[2]+'$'])
 
-#p.s ფასი ძალიან 'მახინჯურად მაქვს' სხვანაირად ვერ მოვახერხე :დ :/
-#სამწუხაროდ 4 მახასიათებელი ერთად მოაქვს, სხვანაირად ვერ მოხერხდა :/ :/
+        
+        # print(name)
+        # print(features)
+
+    sleep(randint(15, 20))
+
+
 
 
 
