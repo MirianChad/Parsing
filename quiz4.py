14cdeab22f9f36a72881e91671d4c2782fb62feb
MirianChad <103271526+MirianChad@users.noreply.github.com>
2022-05-27 18:45:13 +0400

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -7,7 +7,7 @@ from random import randint
 
 file = open("result.csv", "w", encoding="utf-8_sig", newline='\n')
 file_obj = csv.writer(file)
-file_obj.writerow(["დასახელება", "მახასიათებლები"])
+file_obj.writerow(["დასახელება", "მახასიათებლები", "ფასი ლარებში-დოლარებში"])
 
 for ind in range(1,7):
     url = f'https://ss.ge/ka/auto/list/iyideba?Page='+str(ind)+'&AutoManufacturerId=&VehicleModelIds=&VehicleDealTypes=2&YearFrom=&YearTo=&CurrencyId=1&PriceFrom=&PriceTo=&CityIds=&Query=&MillageFrom=&MillageTo=&EngineFrom=&EngineTo=&IsCustomsCleared=&TechnicalInspection=&AutoDealershipId=&El_Windows=false&Hatch=false&CruiseControl=false&StartStopSystem=false&AUX=false&Bluetooth=false&AntiFogHeadlights=false&SeatHeater=false&SmartSeats=false&MultiWheel=false&Signalisation=false&BoardComputer=false&Conditioner=false&ClimateControl=false&RearViewCamera=false&Monitor=false&PanoramicCeiling=false&ParkingControl=false&ABS=false&CentralLocker=false&AutoParking=false&LedHeadlights=false&AdaptedForDisabled=false'
@@ -24,18 +24,24 @@ for ind in range(1,7):
 
     for car in cars_item:
         item_decription = car.find('div', class_='item-decription')
+        price_item = car.find('div', class_='r-item')
+        price = price_item.find('div', class_='price priceGel')
+        price1 = price_item.find('div', class_='price priceUsd')
         name = item_decription.a.text
         features  = item_decription.div.text
+        pricegel_usd = price_item.div.text
 
-        file_obj.writerow([name, features])
+
+
+        file_obj.writerow([name, features, pricegel_usd+'$'])
 
         print(name)
         print(features)
 
-    sleep(randint(15, 20))
+    # sleep(randint(15, 20))
 
-
-#სამწუხაროდ 4 მახასიათებელი ერთად მოაქვს, სხვანაირად ვერ მოხერხდა :/ :/ 
+#p.s ფასი ძალიან 'მახინჯურად მაქვს' სხვანაირად ვერ მოვახერხე :დ :/
+#სამწუხაროდ 4 მახასიათებელი ერთად მოაქვს, სხვანაირად ვერ მოხერხდა :/ :/
 
 
 
